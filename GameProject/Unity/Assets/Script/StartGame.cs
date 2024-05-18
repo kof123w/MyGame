8892df4a92d6a3a124edf4668bed88f1835d2ab8
huangjunquan <1617034185@qq.com>
2024-05-19 07:51:49 +0800

    更改文件目录

---
+++ GameProject/Unity/Assets/Script/StartGame.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+class StartGame : UnitySingleton<StartGame>
+{
+    void Awake()
+    {
+        base.Awake();
+    }
+
+    void Start()
+    {
+        //初始化框架参数
+        // todo
+        
+        //资源管理..版本更新
+        // todo
+        
+        //进入游戏
+        // todo
+    }
+}
