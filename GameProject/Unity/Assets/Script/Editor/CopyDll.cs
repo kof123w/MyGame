5106987d09ab6922dc3852843d482cd54bcad07b
huangjunquan <1617034185@qq.com>
2024-05-23 15:10:16 +0800

    处理Dll复制问题

--- GameProject/Unity/Assets/Script/Editor/CopyDll.cs
+++ GameProject/Unity/Assets/Script/Editor/CopyDll.cs
@@ -9,24 +9,35 @@ public static class CopyDll
     [MenuItem("AotDll/CopyDllToAotDll")]
     public static void MergeSprite()
     {
+        Debug.Log("开始移动dll文件");
         HashSet<string> containDll = new HashSet<string>();
         containDll.Add("Framework.Hotfix.dll");
         containDll.Add("Framework.Hotfix.pdb");
         containDll.Add("Framework.Core.dll");
         containDll.Add("Framework.Core.pdb");
-        string path = "Temp\\Bin\\Debug\\Hotfix";
+        containDll.Add("Framework.Common.dll");
+        containDll.Add("Framework.Common.pdb");
+        containDll.Add("Framework.Model.dll");
+        containDll.Add("Framework.Model.pdb");
+        containDll.Add("Framework.View.dll");
+        containDll.Add("Framework.View.pdb");
+        string path = "Library\\ScriptAssemblies";
         string destinPath = "Assets\\StreamingAssets\\AotDll";
+
+        if (!Directory.Exists(destinPath))
+        {
+            Directory.CreateDirectory(destinPath);
+        }
 
         if (Directory.Exists(destinPath))
         {
             string[] files = Directory.GetFiles(destinPath);
             foreach (string file in files)
-            {
+            { 
                 if (file.Contains(".meta"))
                 {
                     continue;
                 }
-
                 File.Delete(file);
             }
         }
@@ -36,12 +47,11 @@ public static class CopyDll
             string[] files = Directory.GetFiles(path);
             foreach (string file in files)
             {
-                string fileName = Path.GetFileName(file);
-                string destin = Path.Combine(destinPath);
+                string fileName = Path.GetFileName(file);  
                 if (containDll.Contains(fileName))
-                { 
+                {
                     File.Copy(file,string.Format("{0}\\{1}",destinPath,fileName));
-                    Debug.Log("移动文件"+fileName);
+                    Debug.Log($"移动文件{fileName}");
                 }
             }
         }
