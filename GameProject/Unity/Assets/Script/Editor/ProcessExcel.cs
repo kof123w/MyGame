4f55f853c80d8093abb628b8c80192acf9bc809f
huangjunquan <1617034185@qq.com>
2024-05-28 21:09:37 +0800

    提交VO类生成代码

--- GameProject/Unity/Assets/Script/Editor/ProcessExcel.cs
+++ GameProject/Unity/Assets/Script/Editor/ProcessExcel.cs
@@ -6,34 +6,38 @@ using System.Linq;
 using OfficeOpenXml;
 using UnityEditor;
 using Debug = UnityEngine.Debug;
-
+ 
 public static class ProcessExcel
 {
     //配置表路径
-    private const string excelPath = "F:\\GitHubProject\\MyGame\\Excel";
-    private const string outPutPath = "Assets\\StreamingAssets\\Config";
-    private const string excelIndex = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private const string voPath = "Assets\\Script\\Excel\\ConfigVO";
+    private const string ExcelPathGlo = "E:\\UnityWorkSpace\\MyGame\\Excel";
+    private const string OutPutPathGlo = "Assets\\StreamingAssets\\Config";
+    private const string ExcelIndexGlo = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string VoPathGlo = "Assets\\Script\\Excel\\ConfigVO";
+    private const int ExploreRowGlo = 2;  //注释掉的列索引
+    private const int ExploreColumnGlo = 1;  //注释掉的列索引
+    private const int DataTypeColumnGlo = 4; //字段类型
+    private const int DataNameColumnGlo = 5;  //字段名
+    private const int ClientIsUseGlo = 3;     //客户端是否使用字段判断
     
     [MenuItem("ProcessExcel/ReBuildConfig")]
     public static void RebuildConfig()
     {
-        if (!Directory.Exists(outPutPath))
+       
+        if (!Directory.Exists(OutPutPathGlo))
         {
-            Directory.CreateDirectory(outPutPath);
-        }
-
+            Directory.CreateDirectory(OutPutPathGlo);
+        } 
         //保持vo文件夹的干净
-        ClearVoFile();
-
-        if (Directory.Exists(excelPath))
+        ClearVoFile(); 
+        if (Directory.Exists(ExcelPathGlo))
         {
-            string[] files = Directory.GetFiles(excelPath);
+            string[] files = Directory.GetFiles(ExcelPathGlo); 
             for (int i = 0; i < files.Length;i++)
             {
                 string file = files[i];
                 string fileName = Path.GetFileName(file);
-                string excelName = $"{excelPath}\\{fileName}";
+                string excelName = $"{ExcelPathGlo}\\{fileName}";
                 var fileInfo = new FileInfo(excelName);
                 using (ExcelPackage pack = new ExcelPackage(fileInfo))
                 {
@@ -54,27 +58,23 @@ public static class ProcessExcel
                     maxRow = row - 1;
                     
                     //A2是表名
-                    string outPutFileName = currentWorksheet.Cells["B2"].Value as string;
-
-                    //生成配置VO模板
-                    // todo
-                   // GenVoClass(currentWorksheet,maxRow);
+                    string outPutFileName = currentWorksheet.Cells["B1"].Value as string;
+                    
+                    Debug.Log($"{outPutFileName}");
+                    
+                    //生成配置VO模板 
+                    GenVoClass(currentWorksheet,maxRow,outPutFileName);
 
                     //生成配置单例模板
-                    // todo
 
-                    string filePath = $"{outPutPath}\\{outPutFileName}.bin";
-                    //先删掉这个问题
-                    if (File.Exists(filePath))
-                    {
-                        File.Delete(filePath);
-                    }
-
+                    string filePath = $"{OutPutPathGlo}\\{outPutFileName}.bin";
+                  
+                    ClearPathFile(filePath); 
                     //创建出来这个文件
                     if (!File.Exists(filePath))
                     {
                         File.Create(filePath);
-                    }
+                    } 
                 }
             }
         }  
@@ -89,7 +89,7 @@ public static class ProcessExcel
         do
         {
             int index = n % base26;
-            str = $"{excelIndex[index]}{str}";
+            str = $"{ExcelIndexGlo[index]}{str}";
             isCon = n / base26 > 0;
             n = n / base26 - 1;
         } while (isCon);
@@ -99,9 +99,9 @@ public static class ProcessExcel
 
     private static void ClearVoFile()
     {
-        if (Directory.Exists(voPath))
+        if (Directory.Exists(VoPathGlo))
         {
-            string[] voFiles = Directory.GetFiles(voPath);
+            string[] voFiles = Directory.GetFiles(VoPathGlo);
             foreach (string voFile in voFiles)
             { 
                 if (voFile.Contains(".meta"))
@@ -113,16 +113,49 @@ public static class ProcessExcel
         }
     }
 
+    private static void ClearPathFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+    }
+
     //生成vo class
     private static void GenVoClass(ExcelWorksheet worksheet,int maxRow,string tableName)
     {
         
-        string fileName = $"{voPath}\\{tableName}.cs";
+        string fileName = $"{VoPathGlo}\\{tableName}.cs";
         if (!File.Exists(fileName))
         {
             File.Exists(fileName);
+            Debug.Log("创建文件:"+fileName);
         }
-        
-        
-    }
+
+        using (StreamWriter sw = new StreamWriter(fileName))
+        {
+            sw.WriteLine("namespace MyNamespace");
+            sw.WriteLine("{");
+            sw.WriteLine($"  class {tableName}");
+            sw.WriteLine("   {");
+            
+            for (int i = 1; i <= maxRow; i++)
+            {
+                //看看这一列是否被注释掉了
+                var rowStr = GetRowStrByIndex(i);
+                var explore = worksheet.Cells[$"{rowStr}{ExploreRowGlo}"].Value as string;
+                if(explore!=null && explore.Contains("#")) continue;
+                var clientIsUseStr = worksheet.Cells[$"{rowStr}{ClientIsUseGlo}"].Value as string;
+                if(!string.IsNullOrEmpty(clientIsUseStr) && !clientIsUseStr.Contains("C")) continue;
+                
+                string dataType = worksheet.Cells[$"{rowStr}{DataTypeColumnGlo}"].Value as string;
+                string dataName = worksheet.Cells[$"{rowStr}{DataNameColumnGlo}"].Value as string;
+                
+                string pre = " {get;set;}";
+                sw.WriteLine($"    public {dataType} {dataName} {pre}");
+            }
+            sw.WriteLine("   }");
+            sw.WriteLine("}");
+        } 
+    }  
 }
---
+++ GameProject/Unity/Assets/Script/Excel/ConfigVO/TestConfig.cs
@@ -0,0 +1,8 @@
+namespace MyNamespace
+{
+  class TestConfig
+   {
+    public int ID  {get;set;}
+    public string Name  {get;set;}
+   }
+}
